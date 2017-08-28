b8522a1987a0865f13f0c807bbc1646308478cca
Sabyasachi Karati <sabyasachi.karati@gmail.com>
2017-08-28 13:11:32 -0600

    Add files via upload

--- KummerLine2663(260,139)/main.c
+++ KummerLine2663(260,139)/main.c
@@ -13,6 +13,8 @@ int main(){
 	gfe work[4],re[4],temp,x,z,xinvz;
 	gfe54 m54,n54,op54;
 	
+	printf("\nKL266(260,139)===\n");	
+	
 	//sample scalar: you can change it and also can pass it as an argument of the main
 	unsigned char n[33] = { 248, 140, 32, 244, 142, 110, 98, 104, 139, 69, 181, 115, 219, 202, 144, 188, 15, 199, 29, 26, 236, 6, 78, 213, 139, 95, 154, 118, 245, 82, 50, 209, 32};
 	unsigned char op[34];
@@ -36,7 +38,9 @@ int main(){
 		scalar_mult_fixed_base(op, npi, n); //n[0] = op[0];
 	});
 
-	printf("Total CPU cycles for fixed-base scalar multiplication: %.2f.\n", RDTSC_total_clk);
+	printf("Total CPU cycles for fixed-base scalar multiplication Min: %.2f.\n", RDTSC_clk_min);
+	printf("Total CPU cycles for fixed-base scalar multiplication Median: %.2f.\n", RDTSC_clk_median);
+	printf("Total CPU cycles for fixed-base scalar multiplication Max: %.2f.\n", RDTSC_clk_max);
 	print_c(op);
 
 	unsigned char base_rand[64];
@@ -46,7 +50,9 @@ int main(){
 	MEASURE({
 		scalar_mult_var_base(op, base_rand, n); n[0] = op[0];
 	});
-	printf("Total CPU cycles for variable-base scalar multiplication: %.2f.\n", RDTSC_total_clk);
+	printf("Total CPU cycles for variable-base scalar multiplication Min: %.2f.\n", RDTSC_clk_min);
+	printf("Total CPU cycles for variable-base scalar multiplication Median: %.2f.\n", RDTSC_clk_median);
+	printf("Total CPU cycles for variable-base scalar multiplication Max: %.2f.\n", RDTSC_clk_max);
 	print_c(op);
 
 	return 0;
